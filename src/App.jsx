dcfc549b748fcb5b2b1c48a79fe71616904acf2d
Ankit Jangid <ankitjangid8501@gmail.com>
2024-02-07 22:03:35 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import Home from "./components/Home";
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
-     <Home/>
+     <Navbar/>
     </>
   )
 }
