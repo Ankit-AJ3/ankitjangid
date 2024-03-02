a24217dbc9c1a0d84dcaf400723e64e57d57598b
Ankit Jangid <ankitjangid8501@gmail.com>
2024-03-02 23:00:33 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Forms from './components/Forms';
 
 function App() {
 
   return (
     <>
      <Navbar/>
+     <Forms/>
      <Footer/>
     </>
   )
