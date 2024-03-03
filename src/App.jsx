f752c5e88a1bb134eec429d9ab7a8ac339925253
Ankit Jangid <ankitjangid8501@gmail.com>
2024-03-03 21:45:27 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import './App.css'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Forms from './components/Forms';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-     <Navbar/>
-     <Forms/>
-     <Footer/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={ } />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
